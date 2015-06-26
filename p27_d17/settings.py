c7b4e7d3ba4b206f4fa5f1b634503ddf87be458f
marc condon <marc@sfdjango.com>
2015-06-26 12:22:32 -0700

    removed app 'test_app'

--- p27_d17/settings.py
+++ p27_d17/settings.py
@@ -121,7 +121,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'test_app',
+    'hello',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
